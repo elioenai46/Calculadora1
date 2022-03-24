3b1dd92fbd1a92ee0caf4ae47b4c8e9a590b12e3
carlos <carlos.cantrax303@gmail.com>
2022-03-24 12:00:11 -0600

    codigo 1

---
+++ Multiplicacion.java
@@ -0,0 +1,12 @@
+
+public class Multiplicacion {
+	 double multi;
+     
+	    public Multiplicacion(double n1, double n2) {
+	             
+	        super(n1, n2, '*');
+	        this.multi = n1 * n2;
+	        this.setRes(this.multi);
+	    }
+	
+}
