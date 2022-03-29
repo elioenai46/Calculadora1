a21e85c422c0132c37d6c3ddbeb50e14b7a5dc23
MAXPP08 <maxpach02088@gmail.com>
2022-03-28 18:16:33 -0600

    commit 3

--- src/Division.java
+++ src/Division.java
@@ -1,4 +1,15 @@
 
-public class Division {
-
+public class Division extends Operacion{
+	double div = 0;
+	
+	public Division (double n1, double n2) {
+		super(n1, n2,'/');
+		if(n2==0) System.out.println("No se puede dividir entre cero");
+		else this.div= n1 /n2;
+		this.res =this.div;
+	}
+	
+	public void mostrarResultado() {
+		System.out.println(this.n1+""+this.operacion+""+this.n2+" = "+this.res);
+	}
 }
